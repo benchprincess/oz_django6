a4664f610690e7ba726099dbfb949f47a2d4a72e
jiwon hwang <realjiwon98@naver.com>
2024-11-14 13:41:09 +0900

    da password 수정

--- oz_django6/asgi.py
+++ oz_django6/asgi.py
@@ -8,12 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-import django
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oz_django6.settings")
-django.setup()
 application = get_asgi_application()
 
 # temp 주석
---
+++ tests/test_auto_now.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from tabom.models import User
+
+
+class TestAutoNow(TestCase):
+    def test_auto_now_field_is_set_when_save(self) -> None:
+        user = User(name="test")
+        user.save()
+        self.assertIsNotNone(user.updated_at)
+        self.assertIsNotNone(user.created_at)
